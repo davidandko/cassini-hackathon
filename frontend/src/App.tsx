abc610b3d7b48fbc0fab1e1177b5f2721544421e
stefan toskovski <stefantoska84@gmail.com>
2025-05-17 12:53:44 +0200

    add er

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
 import './App.css'
 import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
+import CreateAccountPage from "./pages/create-account";
 
 function App() {
   return (
      <Router>
          <Routes>
-
+             <Route path="/create-account" element={<CreateAccountPage/>}></Route>
          </Routes>
      </Router>
   )
